cf32d5be1f1248ed8b86ed2887d6598201bd6fa8
James Ward <jamesgeorgeward@gmail.com>
2017-03-03 08:59:13 +0000

    Implemented overload for GetIndexItems
    Overload has no string for index name to utilise. Instead it is defaulted to sitecore_web_index which is now stored in a constant.

---
+++ src/Foundation/Search/code/Indexes.cs
@@ -0,0 +1,8 @@
+﻿namespace Helixbase.Foundation.Search
+{
+    public class Indexes
+    {
+        public const string Master = "sitecore_master_index";
+        public const string Web = "sitecore_web_index";
+    }
+}
--- src/Foundation/Search/code/Repositories/ISearchRepository.cs
+++ src/Foundation/Search/code/Repositories/ISearchRepository.cs
@@ -7,6 +7,24 @@ namespace Helixbase.Foundation.Search.Repositories
 {
     public interface ISearchRepository
     {
+        /// <summary>
+        /// Searches for items in the default index, "sitecore_web_index", and returns them as the type specified.
+        /// </summary>
+        /// <typeparam name="T">The model with which to return the items.</typeparam>
+        /// <param name="predicate">The predicate of the search.</param>
+        /// <param name="orderBy">The order of the search.</param>
+        /// <param name="amount">The amount of items to return.</param>
+        /// <returns>Return an enumerable of items of the type specified.</returns>
+        IEnumerable<T> GetIndexItems<T>(Expression<Func<T, bool>> predicate, Expression<Func<T, object>> orderBy = null, int? amount = null) where T : SearchResultItem;
+
+        /// <summary>
+        /// Searches for items in the provided index and returns them as the type specified.
+        /// </summary>
+        /// <typeparam name="T">The model with which to return the items.</typeparam>
+        /// <param name="predicate">The predicate of the search.</param>
+        /// <param name="orderBy">The order of the search.</param>
+        /// <param name="amount">The amount of items to return.</param>
+        /// <returns>Return an enumerable of items of the type specified.</returns>
         IEnumerable<T> GetIndexItems<T>(string indexName, Expression<Func<T, bool>> predicate, Expression<Func<T, object>> orderBy = null, int? amount = null) where T : SearchResultItem;
     }
 }
