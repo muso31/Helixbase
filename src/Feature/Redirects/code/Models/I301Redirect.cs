79dc9fceb3a27e50b86afb2eb13ce1a3abeff662
Neil Shack <nshack31@hotmail.com>
2017-03-08 10:22:12 +0000

    remove line break

--- src/Feature/Redirects/code/Models/I301Redirect.cs
+++ src/Feature/Redirects/code/Models/I301Redirect.cs
@@ -5,7 +5,6 @@ namespace Helixbase.Feature.Redirects.Models
     public interface I301Redirect : ISitecoreItem
     {
         string RequestedURL { get; set; }
-
         ISitecoreItem RedirectItem { get; set; }
     }
 }
