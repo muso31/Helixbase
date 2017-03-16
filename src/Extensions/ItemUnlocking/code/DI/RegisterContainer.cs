c60df1a5cdcee6733b91ae799d328bf8088a874b
James Ward <jamesgeorgeward@gmail.com>
2017-03-16 08:04:24 +0000

    Non-admin unlocking ability added
    Governed by the new permission item:unlock

---
+++ src/Extensions/ItemUnlocking/code/DI/RegisterContainer.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace Helixbase.Extensions.ItemUnlocking.DI
+{
+    public class RegisterContainer : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+        }
+    }
+}
