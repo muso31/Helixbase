3c600bded2de336851abe2affe5a53411bb91e3a
Neil Shack <nshack31@hotmail.com>
2017-11-05 10:14:57 +0000

    Minor comment update

--- src/Foundation/Content/code/Repositories/CmsInfoRepository.cs
+++ src/Foundation/Content/code/Repositories/CmsInfoRepository.cs
@@ -1,5 +1,8 @@
 ﻿namespace Helixbase.Foundation.Content.Repositories
 {
+    /// <summary>
+    /// Retrive Cms information, wrapper for Sitecore Api calls
+    /// </summary>
     public class CmsInfoRepository : ICmsInfoRepository
     {
         public bool IsExperienceEditor => Sitecore.Context.PageMode.IsExperienceEditor;
