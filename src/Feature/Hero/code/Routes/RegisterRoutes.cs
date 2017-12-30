5ddd569c1ae648ea6e667c3b09fe2ca0924a8cb4
Neil Shack <nshack31@hotmail.com>
2017-12-30 08:25:49 +0000

    Add Hero component thumbnail image

--- src/Feature/Hero/code/Routes/RegisterRoutes.cs
+++ src/Feature/Hero/code/Routes/RegisterRoutes.cs
@@ -1,19 +1,20 @@
-﻿using Sitecore.Pipelines;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
+using Sitecore.Pipelines;
 
 namespace Helixbase.Feature.Hero.Routes
 {
     public class RegisterRoutes
     {
         /// <summary>
-        /// This route is NOT required for the Hero feature neither will it work, it is only here as an example of how to register a route
+        ///     This route is NOT required for the Hero feature neither will it work, it is only here as an example of how to
+        ///     register a route. We can also use Sitecore native https://demo.helixbase/api/sitecore/hero/someAction
         /// </summary>
         /// <param name="args"></param>
         public void Process(PipelineArgs args)
         {
             RouteTable.Routes.MapRoute("Feature.Hero", "HeroPath/Hero",
-              new { controller = "Hero", action = "SomeAction" });
+                new {controller = "Hero", action = "SomeAction"});
         }
     }
 }
