f729de0cdac031c1d5e620d0ce2f13e7e0a4d8a1
Neil Shack <nshack31@gmail.com>
2018-07-02 19:20:04 +0100

    Update v5 to nuget.org version

--- src/Foundation/ORM/code/App_Start/GlassMapperScCustom.cs
+++ src/Foundation/ORM/code/App_Start/GlassMapperScCustom.cs
@@ -8,29 +8,32 @@ using IDependencyResolver = Glass.Mapper.Sc.IoC.IDependencyResolver;
 
 namespace Helixbase.Foundation.ORM.App_Start
 {
-    public static  class GlassMapperScCustom
+    public static class GlassMapperScCustom
     {
-		public static IDependencyResolver CreateResolver(){
-			var config = new Glass.Mapper.Sc.Config();
+        public static IDependencyResolver CreateResolver()
+        {
+            var config = new Glass.Mapper.Sc.Config();
 
-			var dependencyResolver = new DependencyResolver(config);
-			// add any changes to the standard resolver here
-			return dependencyResolver;
-		}
+            var dependencyResolver = new DependencyResolver(config);
+            // add any changes to the standard resolver here
+            return dependencyResolver;
+        }
 
-		public static IConfigurationLoader[] GlassLoaders(){			
-			
-			/* USE THIS AREA TO ADD FLUENT CONFIGURATION LOADERS
+        public static IConfigurationLoader[] GlassLoaders()
+        {
+
+            /* USE THIS AREA TO ADD FLUENT CONFIGURATION LOADERS
              * 
              * If you are using Attribute Configuration or automapping/on-demand mapping you don't need to do anything!
              * 
              */
 
-			return new IConfigurationLoader[]{};
-		}
-		public static void PostLoad(){
-			//Remove the comments to activate CodeFist
-			/* CODE FIRST START
+            return new IConfigurationLoader[] { };
+        }
+        public static void PostLoad()
+        {
+            //Remove the comments to activate CodeFist
+            /* CODE FIRST START
             var dbs = Sitecore.Configuration.Factory.GetDatabases();
             foreach (var db in dbs)
             {
@@ -45,8 +48,8 @@ namespace Helixbase.Foundation.ORM.App_Start
             }
              * CODE FIRST END
              */
-		}
-		public static void AddMaps(IConfigFactory<IGlassMap> mapsConfigFactory)
+        }
+        public static void AddMaps(IConfigFactory<IGlassMap> mapsConfigFactory)
         {
             // Add maps here
             // mapsConfigFactory.Add(() => new SeoMap());
