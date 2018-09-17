0a20372dc58386359647b95e0c7d59be8f14de57
Radoslaw Kozlowski <reo.fox@gmail.com>
2018-09-17 22:41:48 +0100

    Add unit tests for Content Repository CreateItem methods

---
+++ src/Foundation/Content/tests/Models/TestItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using Sitecore.Globalization;
+
+namespace Helixbase.Foundation.Content.Tests.Models
+{
+    public class TestItem : ITestItem
+    {
+        public Guid Id { get; set; }
+        public Language Language { get; set; }
+        public int Version { get; set; }
+        public IEnumerable BaseTemplateIds { get; set; }
+        public string TemplateName { get; set; }
+        public Guid TemplateId { get; set; }
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ src/Foundation/Content/tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Helixbase.Foundation.Content.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Helixbase.Foundation.Content.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("709812ab-bb8e-41fa-97c2-9b7d4181b6da")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
