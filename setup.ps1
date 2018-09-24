8966de5e35331b3057b0ddb0744874e2b389dd67
Joe Price <j.price@ry.com>
2018-09-24 16:32:19 +0100

    Setup Script
    Powershell script to run for new Solutions, that renames everything from Helixbase to the required Project Name

---
+++ setup.ps1
@@ -0,0 +1,61 @@
+# Modify Helixbase Solution and Helix Modules to match project specific requirements
+Param(
+        [Parameter(Position=0, Mandatory=$true)]
+        [string]$ProjectName      
+    )
+
+# Variables to change
+$originalName = "Helixbase"
+
+function Rename-Files
+{
+    Param(
+        [Parameter(Position=0, Mandatory=$true)]
+        [string]$StartPath,
+        [Parameter(Position=1, Mandatory=$true)]
+        [string]$OldValue,
+        [Parameter(Position=2, Mandatory=$true)]
+        [string]$NewValue
+    )
+
+    $pattern = "*$OldValue*"
+    $fileItems = Get-ChildItem -File -Path "$StartPath" -Filter $pattern -Recurse -Force | Where-Object { $_.FullName -notmatch "\\(obj|bin)\\?" } 
+    $fileItems | Rename-Item -NewName { $_.Name -replace $OldValue, $NewValue } -Force
+}
+
+function Update-FileContent
+{
+    Param(
+        [Parameter(Position=0, Mandatory=$true)]
+        [string]$StartPath,
+        [Parameter(Position=1, Mandatory=$true)]
+        [string]$OldValue,
+        [Parameter(Position=2, Mandatory=$true)]
+        [string]$NewValue,
+        [Parameter(Position=3, Mandatory=$true)]
+        [string]$FileExtensionsRegex
+    )
+
+    $filesToUpdate = Get-ChildItem -File -Path "$StartPath" -Recurse -Force | Where-Object { ( $_.FullName -notmatch "\\(obj|bin)\\?") -and ($_.Name -match $FileExtensionsRegex) } | Select-String -Pattern $OldValue | Group-Object Path | Select-Object -ExpandProperty Name
+    $filesToUpdate | ForEach-Object { (Get-Content $_ ) -ireplace [regex]::Escape($OldValue), $NewValue | Set-Content $_ -Force }
+}
+
+try {
+    $startPath = $PSScriptRoot
+
+    # Set Variables 
+    $fileExtensionsToUpdateContentRegex = "(.sln|.config|.csproj|.cs|.cshtml|.feature|.js|.nuspec|.role|.sitecore|.targets)$"
+    $oldNamespacePrefix = $originalName
+    $newNamespacePrefix = $ProjectName
+
+
+    Rename-Files -StartPath "$startPath" -OldValue $oldNamespacePrefix -NewValue $newNamespacePrefix
+    Update-FileContent -StartPath "$startPath" -OldValue $oldNamespacePrefix -NewValue $newNamespacePrefix -FileExtensionsRegex $fileExtensionsToUpdateContentRegex
+    
+    Write-Host "Solution and Project references renamed from $originalName to $ProjectName" -ForegroundColor Green
+
+} catch {
+    Write-Host "Modifying the Soluion and Projects failed" -ForegroundColor Red
+    Write-Host $($_.Exception.Message) -ForegroundColor Red
+    Break
+}
