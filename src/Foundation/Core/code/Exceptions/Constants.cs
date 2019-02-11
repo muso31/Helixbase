bf6490657500e54900e642995f28c22f509e539c
Neil Shack <nshack31@gmail.com>
2019-02-11 20:53:36 +0000

    Use static classes to define constants

--- src/Foundation/Core/code/Exceptions/Constants.cs
+++ src/Foundation/Core/code/Exceptions/Constants.cs
@@ -1,10 +1,13 @@
 ﻿namespace Helixbase.Foundation.Core.Exceptions
 {
-    public class Constants
+    /// <summary>
+    /// https://staticreadonly.com/2019/02/06/structures-vs-static-classes-for-sitecore-template-references/
+    /// </summary>
+    public static class Constants
     {
-        public struct InvalidMediatorResponse
+        public static class InvalidMediatorResponse
         {
-            public const string InvalidCodeReturned = "Invalid Mediator Code Returned";
+            public static string InvalidCodeReturned = "Invalid Mediator Code Returned";
         }
     }
 }
