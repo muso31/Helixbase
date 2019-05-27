bf187a46cfe3f611acec211ff278f63a6b76284a
Richard Szalay <richard@richardszalay.com>
2019-05-28 09:10:33 +1000

    Add Helix templates

---
+++ HelixTemplates/Helixbase.Foundation/src/$layerName$/$modulename$/code/DI/RegisterContainer.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace $moduleNamespace$.DI
+{
+    public class RegisterContainer : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+        }
+    }
+}
