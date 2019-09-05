6ac9c710bd0753c266579be78ce8b0cec7ff99e5
Richard Szalay <richard@richardszalay.com>
2019-09-05 15:36:17 +1000

    tmp

---
+++ src/Foundation/Test/code/DI/RegisterContainer.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace Helixbase.Foundation.Test.DI
+{
+    public class RegisterContainer : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+        }
+    }
+}
---
+++ src/Foundation/Test/tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Helixbase.Foundation.Test.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Helixbase.Foundation.Test.Tests")]
+[assembly: AssemblyCopyright("Copyright Helixbase ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("709812ab-bb8e-41fa-97c2-9b7d4181b6da")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
