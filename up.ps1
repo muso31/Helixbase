3e2fbb9dcf1b68ecabf30b35f10da236107d03ad
Joe Price <joe_price_@hotmail.com>
2020-11-02 10:20:27 +0000

    Clean up docker up.ps1

--- up.ps1
+++ up.ps1
@@ -40,25 +40,6 @@ if (-not $status.status -eq "enabled") {
     Write-Error "Timeout waiting for Sitecore CM to become available via Traefik proxy. Check CM container logs."
 }
 
-##TODO THIS ISN'T WORKING.... 
-
-# dotnet sitecore login --cm https://cm.$rootHostDomain/ --auth https://id.$rootHostDomain/ --allow-write true
-# if ($LASTEXITCODE -ne 0) {
-#     Write-Error "Unable to log into Sitecore, did the Sitecore environment start correctly? See logs above."
-# }
-
-# Write-Host "Pushing latest items to Sitecore..." -ForegroundColor Green
-
-# dotnet sitecore ser push
-# if ($LASTEXITCODE -ne 0) {
-#     Write-Error "Serialization push failed, see errors above."
-# }
-
-# dotnet sitecore publish
-# if ($LASTEXITCODE -ne 0) {
-#     Write-Error "Item publish failed, see errors above."
-# }
-
 Write-Host "Opening site..." -ForegroundColor Green
 
 Start-Process https://portainer.$rootHostDomain/
