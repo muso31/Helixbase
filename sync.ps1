42238dddcbb9d13fa880d9869377504b4516ee14
Joe Price <joe_price_@hotmail.com>
2020-11-12 08:56:43 +0000

    Sync Script Update
    Gives user choice of pull or push

--- sync.ps1
+++ sync.ps1
@@ -15,14 +15,33 @@ if($LoginToSitecore) {
     }
 }
 
-Write-Host "Pushing latest items to Sitecore..." -ForegroundColor Green
 
-dotnet sitecore ser push
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "Serialization push failed, see errors above."
-}
 
-dotnet sitecore publish
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "Item publish failed, see errors above."
-}
+$push = New-Object System.Management.Automation.Host.ChoiceDescription '&1.Push', 'Push latest items to Sitecore...'
+$pull = New-Object System.Management.Automation.Host.ChoiceDescription '&2.Pull', 'Pull latest items from Sitecore...'
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($push, $pull)
+$result = $host.ui.PromptForChoice('Sitecore Serialization?', 'How would you like to sync?', $options, 0)
+
+switch ($result)
+    {
+        0 {
+            Write-Host "Pushing latest items to Sitecore..." -ForegroundColor Green
+            dotnet sitecore ser push
+            if ($LASTEXITCODE -ne 0) {
+                Write-Error "Serialization push failed, see errors above."
+            }
+
+            dotnet sitecore publish
+            if ($LASTEXITCODE -ne 0) {
+                Write-Error "Item publish failed, see errors above."
+            }
+
+        }
+        1 {
+            Write-Host "Pulling latest items from Sitecore..." -ForegroundColor Green
+            dotnet sitecore ser pull
+            if ($LASTEXITCODE -ne 0) {
+                Write-Error "Serialization pull failed, see errors above."
+            }
+        }
+    }
